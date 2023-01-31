28c7076d650c21dd044b3cfe859414f62dda9c61
peng duan <sxdfdp@126.com>
2023-01-31 18:30:48 +0800

    添加功能：不在taskbar显示图标，设置窗口类型为desktop

--- pqiv.c
+++ pqiv.c
@@ -6769,7 +6769,7 @@ gboolean window_configure_callback(GtkWidget *widget, GdkEventConfigure *event, 
 		gtk_window_set_keep_above(main_window, TRUE);
 	}
 	if(option_skip_taskbar){
-		gtk_window_set_type_hint(main_window,GDK_WINDOW_TYPE_HINT_NORMAL);
+		gtk_window_set_type_hint(main_window,GDK_WINDOW_TYPE_HINT_DESKTOP);
 		gtk_window_set_skip_taskbar_hint(main_window, TRUE);
 		gtk_window_set_skip_pager_hint(main_window,TRUE);
 	}
